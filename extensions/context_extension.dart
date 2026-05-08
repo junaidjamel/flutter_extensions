75bae65294d141108d5429631664b3f8476c08d6
Junaid Jameel <junaidjameel396@gmail.com>
2026-05-08 10:29:27 +0500

    Context Extension

--- extensions/context_extension.dart
+++ extensions/context_extension.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
 
+extension ContextExtension on BuildContext {
+  // Screen size
+  double get screenHeight => MediaQuery.of(this).size.height;
+  double get screenWidth => MediaQuery.of(this).size.width;
+
+  // Theme
+  TextTheme get textTheme => Theme.of(this).textTheme;
+  ColorScheme get colors => Theme.of(this).colorScheme;
+  ThemeData get theme => Theme.of(this);
+  Brightness get brightness => Theme.of(this).brightness;
+  bool get isDark => brightness == Brightness.dark;
+
+  // Navigation
+  void pop() => Navigator.of(this).pop();
+  void push(Widget page) => Navigator.of(this).push(
+        MaterialPageRoute(builder: (_) => page),
+      );
+}
+
+/// Extension on [BuildContext] for quick access to screen size, theme, and navigation.
+///
+/// Usage:
+/// ```dart
+/// context.screenHeight      // MediaQuery height
+/// context.screenWidth       // MediaQuery width
+/// context.isDark            // true if dark mode
+/// context.colors.primary    // ColorScheme primary
+/// context.pop()             // Navigator.pop
+/// context.push(MyPage())    // Navigator.push
+/// ```
